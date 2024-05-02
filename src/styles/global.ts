4b2a7b981aeef1acfcbebf6912f7b49630435ff0
Julian Schievelkamp <julian.schievelkamp@gmail.com>
2024-05-02 11:39:49 +0200

    test scroll fix

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,6 +14,7 @@ export default createGlobalStyle`
         font-size: 100%;
         width: 100vw;
         height: 100vh;
+        overflow: hidden;
 
         @media ${queries.xl} {
             font-size: 125%;
