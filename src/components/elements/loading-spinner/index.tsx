ad3003853de42011503f83aeea5bd43d9f057122
Julian Schievelkamp <julian.schievelkamp@gmail.com>
2024-05-03 13:09:31 +0200

    update loading spinner color

--- src/components/elements/loading-spinner/index.tsx
+++ src/components/elements/loading-spinner/index.tsx
@@ -1,5 +1,3 @@
-import { StyledPath } from "./styles";
-
 export interface LoadingSpinnerProps {
     size?: string;
 }
@@ -7,7 +5,10 @@ export interface LoadingSpinnerProps {
 const LoadingSpinner = ({ size = "3rem" }: LoadingSpinnerProps) => {
     return (
         <svg viewBox="0 0 100 100" width={size} height={size}>
-            <StyledPath d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+            <path
+                fill="darkgrey"
+                d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
+            >
                 <animateTransform
                     attributeName="transform"
                     attributeType="XML"
@@ -17,7 +18,7 @@ const LoadingSpinner = ({ size = "3rem" }: LoadingSpinnerProps) => {
                     to="360 50 50"
                     repeatCount="indefinite"
                 />
-            </StyledPath>
+            </path>
         </svg>
     );
 };
